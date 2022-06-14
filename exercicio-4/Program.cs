55349edbd6cbe929455bb8a7f84552a2b2253d8a
JuliarGubolin <julia.gubolin@unesp.br>
2022-06-14 17:36:31 -0300

    atualizacao-ex-1-2-3-4

--- exercicio-4/Program.cs
+++ exercicio-4/Program.cs
@@ -6,7 +6,27 @@ namespace exercicio4
     {
         static void Main(string[] args)
         {
-             
+            int dia, mes, ano;
+
+            Console.Write($"Digite um dia: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.Write($"Digite um mes: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.Write($"Digite um ano: ");
+            ano = int.Parse(Console.ReadLine());
+            
+            if((mes < 1 || mes > 12) || (dia > 31 || dia < 1) || (ano > 2000)){
+                Console.Write($"Data inválida, pois ");
+                if(dia > 31 || dia < 1){
+                Console.Write($"o dia {dia} não está entre os valores 1 e 31 ");
+                }if(mes < 1 || mes > 12){
+                    Console.Write($"o mês {mes} não está entre os valores 1 e 12 ");
+                }if(ano > 2000){
+                    Console.Write($"o ano {ano} excede o ano atual 2000.");
+                }
+            }else{
+                Console.Write($"Data válida.");
+            }
         }
     }
 }
